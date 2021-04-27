83a4d3883e22ce94b97d8afca6a0df411588f072
famasmaster78 <jxras11@gmail.com>
2021-04-27 08:31:08 +0200

    opdateret med mysql login info

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const express = require("express");
 const app = express();
 const port = 3050;
 
+// Mysql login info
+// Username: quickDB
+// Password: !Password1!
+
 app.get("/", (req,res) =>{
 
 	res.send("This is a quickSite API endpoint!");
