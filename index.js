d91bee3cb34cddad084ac71bd798dd62d20ac5c8
Alexander <alex191a@gmail.com>
2021-04-27 08:47:36 +0200

    reee

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
 
 // Require
 const express = require("express");
-
+let username; 
+let passwordhash;
 const app = express();
 const port = 3050;
+const routepath = "/username/:username/hash/:password";
 
 app.get("/", (req,res) =>{
 
-	res.send("This is a quickSite API endpoint!");
+	//res.send("This is a quickSite API endpoint!");
+	res.send(req.query.prams);
+	if(req.query.username||req.query.passwordhash)
+	res.redirect("Loginsite", token)
 
 })
 
+
+
 // Start express server
-app.listen(port, () => console.log("App started!"));
+app.listen(port, () => console.log("App started!"))
