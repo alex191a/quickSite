f723b5c89cbf7467d01f058620dc27f47d892590
Alexander <alex191a@gmail.com>
2021-04-27 11:10:59 +0200

    hello

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 
 // Require
 const express = require("express");
-let username; 
-let passwordhash;
+const mysql = require('mysql')
+
 const app = express();
 const port = 3050;
 const routepath = "/username/:username/hash/:password";
@@ -25,13 +25,47 @@ app.use(session({
 // Server: famas.ml
 // Database: quicksite
 
+const db = 'quicksite';
+const dbusername = 'quickDB';
+const dbPassword = '!Password1!';
+const server = 'famas.ml'
+const conn = mysql.createConnection({
+	host: server,
+	user: dbusername,
+	password: dbPassword,
+	database: db
+})
+
 app.get("/", (req,res) =>{
-
+conn.connect()
 	//res.send("This is a quickSite API endpoint!");
-	res.send(req.query.prams);
-	if(req.query.username||req.query.passwordhash)
-	res.redirect("Loginsite", token)
+	let token
+	let username = "undefined"
+	if(req.query.username){
+		username = req.query.username 
+	}
+	let authenticate;
+	authenticate= conn.query(`SELECT password FROM Users WHERE Users.username = "${username}"`,function (error, results){
+		if(error) throw error;
+	 }).RowDataPacket.toString()
+	 
+	res.send (authenticate);
+	if(req.query.passwordhash==authenticate){
+		conn.query(`SELECT id FROM Users WHERE username = "${username}"`,function (error, results){
+			if(error) throw error;
+			token = results[0]
+		});
+	}
+	else{
+		//res.redirect("/");
+		res.send("ass")
+	}
+	if(token){
+		res.send("jonas er Ass")
+		//res.redirect("yoursite.js/"+token);
+	}
 
+	
 })
 
 
