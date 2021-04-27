db4a9eb81cbcff4646e56689176cec36b3e3d584
famasmaster78 <jxras11@gmail.com>
2021-04-27 08:50:34 +0200

    Opdateret db info

--- index.js
+++ index.js
@@ -2,16 +2,47 @@
 // Require
 const express = require("express");
 
+// Express
 const app = express();
 const port = 3050;
+
+// Express sessions
+const session = require("express-session");
+
+// Opstart sessions
+app.set('trust proxy', 1) // trust first proxy
+app.use(session({
+	secret: '!Password1!',
+	resave: false,
+	saveUninitialized: true,
+	cookie: { secure: true }
+}))
 
 // Mysql login info
 // Username: quickDB
 // Password: !Password1!
+// Server: famas.ml
+// Database: quicksite
 
 app.get("/", (req,res) =>{
 
-	res.send("This is a quickSite API endpoint!");
+	// Tjek om brugeren er logget ind
+	if (!req.session.isLoggedIn) {
+		res.send("Du er ikke logget ind!");
+	}else {
+		res.send("This is a quickSite API endpoint!" + req.query.peter);
+	}
+
+})
+
+// Test
+app.get("/username/:username/hash/:password/", (req,res) => {
+
+	// Opret vars
+	var username = req.params.username;
+	var password = req.params.password;
+
+	res.send(username + "-" + password);
 
 })
 
