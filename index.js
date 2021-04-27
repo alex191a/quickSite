3a2eba4939c3b5ae8103a9c0a22fbb4a9367c8ad
famasmaster78 <jxras11@gmail.com>
2021-04-27 10:08:28 +0200

    signup and login now working

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ const port = 3050;
 // Express sessions
 const session = require("express-session");
 
+// Bcrypt til hashing af passwords.
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+
 // Body parser m.m. til POST info
 var bodyParser = require('body-parser');
 var multer = require('multer');
@@ -33,11 +37,30 @@ app.use(session({
 // Server: famas.ml
 // Database: quicksite
 
+// Mysql
+var mysql = require('sync-mysql');
+var conn = new mysql({
+	host: 'famas.ml',
+	user: 'quickDB',
+	password: '!Password1!',
+	database: "quicksite"
+});
+
+// get the client
+const mysql2 = require('mysql2');
+// create the connection to database
+const conn2 = mysql2.createConnection({
+	host: 'famas.ml',
+	user: 'quickDB',
+	password: '!Password1!',
+	database: 'quicksite'
+});
+
 app.get("/", (req,res) =>{
 
 	// Tjek om logget ind
 	if (req.session.loggedIn) {
-		res.send("This is a quickSite API endpoint!");
+		res.send(`This is a quickSite API endpoint! - ${req.session.username}`);
 	}else {
 		res.send("You are not logged in!");
 	}
@@ -48,12 +71,114 @@ app.get("/session", (req,res) => {
 
 	// Start session
 	req.session.loggedIn = true;
+	req.session.username = "mortenand123"
 
-	// Send
-	res.send("Session started!");
+	// Send tilbage til startsiden
+	res.redirect("/");
 
+})
+
+// Post login
+app.post("/auth", (req,res) => {
+
+	// Opret echo som sendes om svar til sidst
+	var echo = {
+		err: "",
+		errCode: 0,
+		success: false,
+		status: ""
+	}
+
+	// Post login params
+	var username = req.body.username;
+	var password = req.body.password;
+
+	// Tjek om dette brugernavn findes
+	var result = conn.query(`SELECT * FROM Users WHERE username = "${username}" LIMIT 1`);
+
+	// Tjek om denne bruger findes i result array
+	if (result.length > 0) {
+		
+		// Opret variabler
+		var passwordHash = result[0].password;
+
+		// Tjek om passwords stemmer overens
+		if (bcrypt.compareSync(password, passwordHash)) {
+
+			// Opdater echo
+			echo.success = true;
+			echo.status = "Login found and password correct!"
+
+		}else {
+
+			// Opdater echo
+			echo.success = false;
+			echo.status = "Login found but password is incorrect!";
+
+		}
+
+	}else {
+		
+		// Opdater echo
+		echo.success = false;
+		echo.status = "Login not found!";
+
+	}
+	
+	// Send echo
+	res.send(echo);
+
+})
+
+// Opret ny bruger
+app.post("/signup", (req,res) => {
+
+	// Opret echo som sendes om svar til sidst
+	var echo = {
+		err: "",
+		errCode: 0,
+		success: false,
+		status: ""
+	}
+
+	// Opret variabler
+	var username = req.body.username;
+	var password = req.body.password;
+
+	// Log
+	console.log(`New user request: username: ${username} - pass: ${password}`)
+
+	// Hash password
+	const salt = bcrypt.genSaltSync(saltRounds);
+	const passwordHash = bcrypt.hashSync(password, salt);
+
+	// Opret ny bruger i DB.
+	conn2.query(`INSERT INTO Users (username, password) VALUES ("${username}", "${passwordHash}")`, 
+	(err, results, fields) => {
+		if (err) {
+		
+			// Opdater echo med errors
+			echo.err = err;
+			echo.errCode = 500;
+			echo.success = false;
+
+			res.send(echo);
+		
+			throw err;
+		}
+
+		console.log("Results", results);
+		console.log("Fields", fields);
+
+		// Opdater echo
+		echo.success = true;
+		echo.status = "User created with username: " + username;
+
+		res.send(echo);
+
+	})
 
 })
 
 // Start express server
-app.listen(port, () => console.log("App started!"))
+app.listen(port, () => console.log(`App started on port: ${port}!`))
